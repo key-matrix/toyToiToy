378e413385dd573088224803f65221f213425b73
Kaizer <matrix.manage.japan@gmail.com>
2024-04-04 14:49:38 +0900

    UI修正-HOME画面変更(アクティブユーザ,お気に) ListView.builderへ修正04+URL修正

--- lib/view/view_widget/common_widget/parts/list/image_urls_sample.dart
+++ lib/view/view_widget/common_widget/parts/list/image_urls_sample.dart
@@ -9,10 +9,10 @@ class ImageUrls {
     "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl01/assets/imgs/user_icons/onlineUser05.jpeg?raw=true",
   ];
   static List<String> favoriteUserList = [
-    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl06/assets/imgs/user_icons/onlineUser01.jpeg?raw=true",
-    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl07/assets/imgs/user_icons/onlineUser02.jpeg?raw=true",
-    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl08/assets/imgs/user_icons/onlineUser03.jpeg?raw=true",
-    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl09/assets/imgs/user_icons/onlineUser04.jpeg?raw=true",
-    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl10/assets/imgs/user_icons/onlineUser05.jpeg?raw=true",
+    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl01/assets/imgs/user_icons/onlineUser06.jpeg?raw=true",
+    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl01/assets/imgs/user_icons/onlineUser07.jpeg?raw=true",
+    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl01/assets/imgs/user_icons/onlineUser08.jpeg?raw=true",
+    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl01/assets/imgs/user_icons/onlineUser09.jpeg?raw=true",
+    "https://github.com/key-matrix/toyToiToy/blob/UI-mock-Impl01/assets/imgs/user_icons/onlineUser10.jpeg?raw=true",
   ];
 }
