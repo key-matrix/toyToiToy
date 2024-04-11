d980eb5787f4d848a51e8d73508e593b9912ef14
Kaizer <matrix.manage.japan@gmail.com>
2024-04-11 12:45:40 +0900

    UI修正-フォント変更 　動作確認OK-ダークテーマのfontFamily指定不可っぽい為,ダークテーマからライトへ変更

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:marie_app/providers.dart';
 import 'package:marie_app/view/view_widget/screen/login/login_top.dart';
 
-
 void main() {
   runApp(
     //監視対象
@@ -24,9 +23,12 @@ class MyApp extends ConsumerWidget {
       //プロバイダ参照
       title: ref.watch(appNameProvider),
       //ダークテーマ
-      theme: ThemeData.dark(
-          // primarySwatch: Colors.teal,
-          ),
+      theme: ThemeData(
+        fontFamily: "JpFont", //Textの字体や大きさ
+
+        // primarySwatch: Colors.teal,
+        // fontFamily: "KleeOne",//Textの字体や大きさ
+      ),
       //ルート画面
       home: const LoginTop(), //  AppTop()から変更
     );
