2e82d19c5eee994ce3995af931414fb75a853ef4
Kaizer <matrix.manage.japan@gmail.com>
2024-04-11 12:14:08 +0900

    UI修正-マイページ画面,動作確認OK)

--- lib/view/view_widget/screen/common_widget/mypage/my_page.dart
+++ lib/view/view_widget/screen/common_widget/mypage/my_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,130 +10,135 @@ class MyPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final userName = ref.watch(loginUserProvider);
 
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 3),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 3),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 40.0, bottom: 10),
-            child: Row(
-              children: [
-                // プロフィール画像
-                const CircleAvatar(
-                  radius: 35.0,
-                  backgroundColor: Colors.deepPurpleAccent,
-                  backgroundImage:
-                      AssetImage('assets/imgs/profile_icon_round.png'),
-                  // backgroundImage: Image.asset('assets/imgs/profile_icon_round.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 60.0),
-                  child: Column(
-                    children: [
-                      // ユーザー名
-                      Text(
-                        userName["name"]!,
-                        style: const TextStyle(fontSize: 20.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 40.0, bottom: 10),
+                child: Row(
+                  children: [
+                    // プロフィール画像
+                    const CircleAvatar(
+                      radius: 35.0,
+                      backgroundColor: Colors.deepPurpleAccent,
+                      backgroundImage:
+                          AssetImage('assets/imgs/profile_icon_round.png'),
+                      // backgroundImage: Image.asset('assets/imgs/profile_icon_round.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 60.0),
+                      child: Column(
+                        children: [
+                          // ユーザー名
+                          Text(
+                            userName["name"]!,
+                            style: const TextStyle(fontSize: 20.0),
+                          ),
+                          // メールアドレス
+                          Text(
+                            userName["email"]!,
+                            style: const TextStyle(fontSize: 11.0),
+                          ),
+                        ],
                       ),
-                      // メールアドレス
-                      Text(
-                        userName["email"]!,
-                        style: const TextStyle(fontSize: 11.0),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              const Divider(
+                height: 0,
+                endIndent: 0,
+                color: Colors.white70,
+              ),
+
+              // todo 出品,募集　Btn
+
+              Expanded(
+                child: ListView(
+                  children: [
+                    // メニューリスト
+
+                    ListTile(
+                      title: const Text('お気に入り'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    // 履歴
+                    ListTile(
+                      title: const Text('取引履歴'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('売り上げ確認'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('振り込み申請'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('興味ありUser'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('利用ガイド'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('問い合わせ'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('意見書'),
+                      onTap: () {
+                        // 履歴画面へ遷移
+                      },
+                    ),
+
+                    // 設定
+                    ListTile(
+                      title: const Text('設定'),
+                      onTap: () {
+                        // 設定画面へ遷移
+                      },
+                    ),
+                    // ログアウト
+                    ListTile(
+                      title: const Text('ログアウト'),
+                      onTap: () {
+                        // ログアウト処理
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('退会'),
+                      onTap: () {
+                        // ログアウト処理
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          const Divider(
-            height: 0,
-            endIndent: 0,
-            color: Colors.white70,
-          ),
-
-          // todo 出品,募集　Btn
-
-          Expanded(
-            child: ListView(
-              children: [
-                // メニューリスト
-
-                ListTile(
-                  title: const Text('お気に入り'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                // 履歴
-                ListTile(
-                  title: const Text('取引履歴'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                ListTile(
-                  title: const Text('売り上げ確認'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                ListTile(
-                  title: const Text('振り込み申請'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                ListTile(
-                  title: const Text('興味ありUser'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                ListTile(
-                  title: const Text('利用ガイド'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                ListTile(
-                  title: const Text('問い合わせ'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-                ListTile(
-                  title: const Text('意見書'),
-                  onTap: () {
-                    // 履歴画面へ遷移
-                  },
-                ),
-
-                // 設定
-                ListTile(
-                  title: const Text('設定'),
-                  onTap: () {
-                    // 設定画面へ遷移
-                  },
-                ),
-                // ログアウト
-                ListTile(
-                  title: const Text('ログアウト'),
-                  onTap: () {
-                    // ログアウト処理
-                  },
-                ),
-                ListTile(
-                  title: const Text('退会'),
-                  onTap: () {
-                    // ログアウト処理
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
